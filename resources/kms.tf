a72a074de6377597e011fdc49346d42df82c73b5
jaynish-chhabhaiya-prominentpixel <jaynish.chhabhaiya@prominentpixel.com>
2026-05-23 18:09:31 +0530

    initial commmit

--- resources/kms.tf
+++ resources/kms.tf
@@ -1,9 +1,11 @@
+# KMS Key for DynamoDB encryption
+
 module "common_aws_kms_key" {
-  source                  = "git@github.com:CA-Bridge/ccaas-terraform-modules-wrapper.git//terraform-aws-kms-wrapper?ref=v1.0.8"
-  name                    = [format("%s-kms-common-key-%s-%s", var.company_prefix, local.region_prefix, var.env)]
-  description             = "Encryption Decryption Key"
+  source                  = "git@github.com:kiro-krafters/kk-terraform-modules-wrapper.git//terraform-aws-kms-wrapper?ref=v1.0.0"
+  name                    = [format("%s-kms-dynamodb-key-%s-%s", var.company_prefix, local.region_prefix, var.env)]
+  description             = "KMS key for DynamoDB table encryption"
   rotation_period_in_days = 90
   multi_region            = false
-  key_statements          = local.key_statements
+  key_statements          = []
   tags                    = local.tags
 }
--- resources/variables.tf
+++ resources/variables.tf
@@ -1,16 +1,8 @@
+# Required variables for DynamoDB tables
+
 variable "company_prefix" {
   type        = string
-  description = "Company prefix."
-}
-variable "organization" {
-  description = "Organization name in GitHub."
-  type        = string
-  default     = "CA-Bridge"
-}
-variable "application" {
-  type        = string
-  description = "Application name."
-  default     = "connect"
+  description = "Company prefix for resource naming."
 }
 
 variable "company" {
@@ -21,14 +13,9 @@ variable "company" {
 variable "project" {
   type        = string
   description = "Project name."
-  default     = "CCaaS"
+  default     = "KK"
 }
 
-variable "instance_storage_configs" {
-  description = "Map of storage configurations for the Connect instance"
-  type        = map(any)
-  default     = {}
-}
 variable "region" {
   type        = string
   description = "AWS region."
@@ -36,71 +23,21 @@ variable "region" {
 
 variable "env" {
   type        = string
-  description = "Deployment environment."
-}
-
-variable "repo_url" {
-  type        = string
-  description = "Repository URL."
-  default     = "https://github.com/CA-Bridge/ccaas-terraform-connect.git"
+  description = "Deployment environment (dev, stg, prd)."
 }
 
 variable "account_number" {
-  description = "Account Number."
   type        = string
-  default     = null
+  description = "AWS Account Number."
 }
 
 variable "role_name" {
-  description = "Role name."
   type        = string
-  default     = "shared-github-oidc-role"
-}
-
-variable "create_instance" {
-  type        = bool
-  description = "Set to true for the first-time deployment to create the instance. Set to false for subsequent deployments to use the existing instance."
-  default     = false
+  description = "IAM role name for deployment."
 }
 
 variable "is_primary" {
   type        = bool
-  description = "Set to true for us-east-1. Set to false for us-west-2."
+  description = "Set to true for primary region (us-east-1). DynamoDB tables will only be created in primary region."
   default     = false
-}
-variable "cases_domain_id" {
-  type        = string
-  description = "Cases domain ID (set in terraform.tfvars)"
-  default     = null
-}
-variable "enabled" {
-  type        = bool
-  description = "Whether the CloudFront distribution is enabled."
-  default     = true
-}
-
-variable "comment" {
-  type        = string
-  description = "Comment(discription) for the CloudFront distribution."
-  default     = "CloudFront distribution for SMS Sender"
-}
-variable "default_root_object" {
-  type        = string
-  description = "Default root object for the CloudFront distribution."
-  default     = "index.html"
-}
-variable "is_ipv6_enabled" {
-  type        = bool
-  description = "Whether IPv6 is enabled for the CloudFront distribution."
-  default     = true
-}
-
-variable "outbound_caller_id_number_id" {
-  description = "Outbound caller ID number ID."
-  type        = string
-}
-
-variable "sms_origination_number" {
-  description = "SMS origination number."
-  type        = string
 }
